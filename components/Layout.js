ce76f3c3d2996777fbd3b69ebb19541848639ed8
kaylem williams <willkj8@student.op.ac.nz>
2022-09-21 15:38:47 +1200

    init

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+
+import Navbar from './NavBar'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout;
---
+++ components/NavBar.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About Us</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact Us</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
